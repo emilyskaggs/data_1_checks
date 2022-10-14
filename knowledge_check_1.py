cf95e5c7e56a4dc34e2a38368a2088aecbae3fb1
emilyskaggs <115664632+emilyskaggs@users.noreply.github.com>
2022-10-14 19:23:36 -0400

    Add files via upload

---
+++ knowledge_check_1.py
@@ -0,0 +1,8 @@
+
+fruit=["banana", "apple", "orange"]
+grocery_budget= {"fruit":12, "veggies":13, "dairy":13}
+tuple = ("i", "think", "this", "will", "work")
+print("hello world!")
+print(fruit[2])
+print(grocery_budget["fruit"])
+print(tuple[3])
